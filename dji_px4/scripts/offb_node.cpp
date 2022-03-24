2a8fd63f576e3a1ed5c0d04f9f650f43fc20e32e
Marsilanding <55947809+Marsilanding@users.noreply.github.com>
2022-03-24 09:46:38 +0100

    fixed position control bug

--- dji_px4/scripts/offb_node.cpp
+++ dji_px4/scripts/offb_node.cpp
@@ -4,10 +4,15 @@
  * Stack and tested in Gazebo SITL
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+
 
 
 #include <sensor_msgs/Joy.h>
@@ -23,12 +28,21 @@
 #include <mavros_msgs/WaypointReached.h>
 #include <mavros_msgs/GlobalPositionTarget.h>
 
+#include <uav_abstraction_layer/AttSetpoint.h>
+#include <uav_abstraction_layer/AttRateSetpoint.h>
+#include <uav_abstraction_layer/SetHome.h>
+#include <uav_abstraction_layer/TakeOff.h>
 
-int ctrl_flag = 2;
+#include <uav_abstraction_layer/Land.h>
 
-mavros_msgs::State current_state;
+
+int ctrl_flag = 4;
+double takeoff_height = 2.0;
+bool blocking = 1;
+
+mavros_msgs::State mavros_state_;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
-    current_state = *msg;
+    mavros_state_ = *msg;
 }
 
 /*Callback de GPS*/
@@ -43,42 +57,56 @@ void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
     current_pose = *msg;
 }
 
-sensor_msgs::Joy joy_command;
-std_msgs::Float64 joy_x;
-std_msgs::Float64 joy_y;
+/*sensor_msgs::Joy joy_commandXY;
+sensor_msgs::Joy joy_commandZYaw;*/
+sensor_msgs::Joy joy_command_rec;
+std_msgs::Float64 joy_x_rec;
+std_msgs::Float64 joy_y_rec;
+std_msgs::Float64 joy_z_rec;
+std_msgs::Float64 joy_yaw_rec;
+std_msgs::Float64 flag_msg; /*flag de /dji_sdk/flight_control_setpoint_generic*/
+int flag = 0; /*flag en entero*/
 
-void command_rec(const sensor_msgs::Joy::ConstPtr& msg){
-    joy_command = *msg;
-    joy_x.data = joy_command.axes[0];
-    joy_y.data = joy_command.axes[1];
+
+
+/*Comando completo para caso real*/
+
+void dji_setpoint_generic(const sensor_msgs::Joy::ConstPtr& msg){
+    joy_command_rec = *msg;
+    joy_x_rec.data = joy_command_rec.axes[0];
+    joy_y_rec.data = joy_command_rec.axes[1];
+    joy_z_rec.data = joy_command_rec.axes[2];
+    joy_yaw_rec.data = joy_command_rec.axes[3];
+    flag_msg.data = joy_command_rec.axes[4]; /*guardo valor del flag*/
+    flag = (int)flag_msg.data;
+    ctrl_flag = 0;   
 }
 
-mavros_msgs::WaypointReached reached_wp;
-void wp_reached(const mavros_msgs::WaypointReached::ConstPtr& msg){
-    reached_wp = *msg;
-    ROS_INFO("WP alcanzado");
+void dji_enuposition_yaw(const sensor_msgs::Joy::ConstPtr& msg){
+    joy_command_rec = *msg;
+    joy_x_rec.data = joy_command_rec.axes[0];
+    joy_y_rec.data = joy_command_rec.axes[1];
+    joy_z_rec.data = joy_command_rec.axes[2];
+    joy_yaw_rec.data = joy_command_rec.axes[3];
+    ctrl_flag = 1;   
 }
 
-/*Para cambiar modo de vuelo (extraído de UAL)*/
-void setFlightMode(const std::string& _flight_mode) {
-    mavros_msgs::SetMode flight_mode_service;
-    flight_mode_service.request.base_mode = 0;
-    flight_mode_service.request.custom_mode = _flight_mode;
-    // Set mode: unabortable?
-    while (mavros_state_.mode != _flight_mode && ros::ok()) {
-        if (!flight_mode_client_.call(flight_mode_service)) {
-            ROS_ERROR("Error in set flight mode [%s] service calling!", _flight_mode.c_str());
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
-#ifdef MAVROS_VERSION_BELOW_0_20_0
-        ROS_INFO("Set flight mode [%s] response.success = %s", _flight_mode.c_str(), \
-            flight_mode_service.response.success ? "true" : "false");
-#else
-        ROS_INFO("Set flight mode [%s] response.success = %s", _flight_mode.c_str(), \
-            flight_mode_service.response.mode_sent ? "true" : "false");
-#endif
-        ROS_INFO("Trying to set [%s] mode; mavros_state_.mode = [%s]", _flight_mode.c_str(), mavros_state_.mode.c_str());
-    }
+void dji_enuvelocity_yawrate(const sensor_msgs::Joy::ConstPtr& msg){
+    joy_command_rec = *msg;
+    joy_x_rec.data = joy_command_rec.axes[0];
+    joy_y_rec.data = joy_command_rec.axes[1];
+    joy_z_rec.data = joy_command_rec.axes[2];
+    joy_yaw_rec.data = joy_command_rec.axes[3]; 
+    ctrl_flag = 2;  
+}
+
+void dji_rollpitch_yawrate_z(const sensor_msgs::Joy::ConstPtr& msg){
+    joy_command_rec = *msg;
+    joy_x_rec.data = joy_command_rec.axes[0];
+    joy_y_rec.data = joy_command_rec.axes[1];
+    joy_z_rec.data = joy_command_rec.axes[2];
+    joy_yaw_rec.data = joy_command_rec.axes[3]; 
+    ctrl_flag = 3;  
 }
 
 int main(int argc, char **argv)
@@ -86,96 +114,198 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+    std::string dji_ns = "dji";
+
+    std::string set_generic_topic = dji_ns + "/flight_control_setpoint_generic";
+    std::string set_enuposition_topic = dji_ns + "/flight_control_setpoint_ENUposition_yaw";
+    std::string set_enuvelocity_topic = dji_ns + "/flight_control_setpoint_ENUvelocity_yawrate";
+    std::string set_rollpitch_yaw_topic = dji_ns + "/flight_control_setpoint_rollpitch_yawrate_zposition";
+
+    /*Creamos clientes para llamadas a servicios de UAL*/
+
+    ros::ServiceClient takeoff_client = nh.serviceClient<uav_abstraction_layer::TakeOff>
+            ("/ual/take_off");
+
+    ros::ServiceClient land_client = nh.serviceClient<uav_abstraction_layer::Land>
+            ("/ual/land");
+
+    ros::ServiceClient sethome_client = nh.serviceClient<uav_abstraction_layer::SetHome>
+            ("/ual/set_home");
+
+
+    ros::Subscriber mavros_state = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
 
     ros::Subscriber gps_sub = nh.subscribe<sensor_msgs::NavSatFix> /*Global Position*/
             ("mavros/global_position/global", 10, gps_cb);
 
     ros::Subscriber local_pos_sub = nh.subscribe<geometry_msgs::PoseStamped> /*Local Position*/
-            ("mavros/local_position/pose", 10, pose_cb);
+            ("ual/pose", 10, pose_cb);
 
-    ros::Subscriber wp_reached_sub = nh.subscribe<mavros_msgs::WaypointReached> /*WP Reached*/
-            ("mavros/mission/reached", 10, wp_reached); 
 
-    ros::Subscriber sp_command_sub = nh.subscribe<sensor_msgs::Joy> 
-            ("/joy", 10, command_rec);        
+    /*Suscriptores para caso real*/
+    ros::Subscriber dji_setpoint_generic_sub = nh.subscribe<sensor_msgs::Joy>
+        ("/dji_sdk/flight_control_setpoint_generic", 10, dji_setpoint_generic);
+
+    ros::Subscriber dji_enuposition_yaw_sub = nh.subscribe<sensor_msgs::Joy>
+        ("/dji_sdk/flight_control_setpoint_ENUposition_yaw", 10, dji_enuposition_yaw);
+
+    ros::Subscriber dji_enuvelocity_yawrate_sub = nh.subscribe<sensor_msgs::Joy>
+        ("/dji_sdk/flight_control_setpoint_ENUvelocity_yawrate", 10,  dji_enuvelocity_yawrate);
+
+    ros::Subscriber dji_rollpitch_yawrate_zpos_sub = nh.subscribe<sensor_msgs::Joy>
+        ("/dji_sdk/flight_control_setpoint_rollpitch_yawrate_zposition", 10, dji_rollpitch_yawrate_z);
 
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("offb_node/setpoint_position/local", 10);
+            ("/ual/set_pose", 10);
 
     ros::Publisher local_vel_pub = nh.advertise<geometry_msgs::TwistStamped>
-            ("mavros/setpoint_velocity/cmd_vel", 10);
+            ("/ual/set_velocity", 10);
 
-    ros::Publisher local_attitude_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_attitude/attitude", 10);
+    ros::Publisher local_attitude_pub = nh.advertise<uav_abstraction_layer::AttSetpoint>
+            ("/ual/set_attitude", 10);
 
-    ros::Publisher global_pos_pub = nh.advertise<mavros_msgs::GlobalPositionTarget>
-            ("mavros/setpoint_raw/global", 10);
+    ros::Publisher local_attitude_rate_pub = nh.advertise<uav_abstraction_layer::AttRateSetpoint>
+            ("/ual/set_attitude_rate", 10);
+
+    /*Publicadores que envian comandos*/
+    ros::Publisher dji_enuposition_yaw_pub = nh.advertise<sensor_msgs::Joy>
+            ("/dji_sdk/flight_control_setpoint_ENUposition_yaw", 10);
+
+    ros::Publisher dji_enuvelocity_yawrate_pub = nh.advertise<sensor_msgs::Joy>
+            ("/dji_sdk/flight_control_setpoint_ENUvelocity_yawrate", 10);
+
+    ros::Publisher dji_rollpitch_yawrate_zpos_pub = nh.advertise<sensor_msgs::Joy>
+            ("/dji_sdk/flight_control_setpoint_rollpitch_yawrate_zposition", 10);
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
-    // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
-        ros::spinOnce();
-        rate.sleep();
-    }
-
+    
     /*Given wp*/
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 2;
-    pose.pose.position.y = 2;
-    pose.pose.position.z = 2;
-    pose.pose.orientation.z = 0.5;
-
     geometry_msgs::TwistStamped twist;
-    twist.twist.linear.x = 2;
-    twist.twist.linear.y = 2;
-    twist.twist.linear.z = 0;
-    twist.twist.angular.z = 0;
-
-    geometry_msgs::PoseStamped orientation;
-    orientation.pose.orientation.x = 0;
-    orientation.pose.orientation.y = 0;
-    orientation.pose.orientation.z = 0;
-
-    setFlightMode(OFFBOARD);
-    ros::Time last_request = ros::Time::now();
-
-    int flag = 0;
+    uav_abstraction_layer::AttSetpoint attitude;
+    uav_abstraction_layer::AttRateSetpoint attitude_rate;
+    uav_abstraction_layer::TakeOff::Request takeoff_req;
+    uav_abstraction_layer::TakeOff::Response takeoff_resp;
+    takeoff_req.height = takeoff_height;
+    takeoff_req.blocking = blocking;
 
 
+    int r = 0;
+    int c = 0;
 
     while(ros::ok()){
 
         if(ctrl_flag == 0){
-            pose.pose.position.x = pose.pose.position.x + joy_x.data; /*joy_command.axes[0]*/
-            pose.pose.position.y = pose.pose.position.y + joy_y.data;
-            pose.pose.position.z = 2;  
-            local_pos_pub.publish(pose);
+            ROS_INFO("Control generic...");
+
+            /*LECTURA FLAG HORIZONTAL*/
+            r = flag%64;
+            c = flag/64;
+            if(c == 0){
+                attitude.orientation.x = current_pose.pose.orientation.x + joy_x_rec.data; /*roll*/
+                attitude.orientation.y = current_pose.pose.orientation.y + joy_y_rec.data; /*pitch*/
+            }
+            else if(c == 1){
+                twist.twist.linear.x = joy_x_rec.data; /*velocidad en x*/
+                twist.twist.linear.y = joy_y_rec.data; /*velocidad en y*/
+            }
+            else if(c == 2){
+                pose.pose.position.x = current_pose.pose.position.x + joy_x_rec.data; /*X offset*/
+                pose.pose.position.y = current_pose.pose.position.y; + joy_y_rec.data; /*Y offset*/
+            }
+            else {
+                pose.pose.position.x = current_pose.pose.position.x + joy_x_rec.data; /*X offset*/
+                pose.pose.position.y = current_pose.pose.position.y; + joy_y_rec.data; /*Y offset*/
+            }
+
+            /*LECTURA DE FLAG VERTICAL*/
+            c = r/16;
+            r = r%16;
+
+            if(c == 0){
+                twist.twist.linear.z = joy_z_rec.data; /*velocidad en z*/
+            }
+            else if(c == 1){
+                pose.pose.position.z = joy_z_rec.data; /*height*/
+            }
+            else{
+                attitude.thrust = joy_z_rec.data/100; /*thrust (pasando de 0-100 a 0-1)*/
+            }
+
+            /*LECTURA DE FLAG YAW*/
+            c = r/8;
+            r = r%8;
+
+            if(c == 0){
+                pose.pose.orientation.z = joy_yaw_rec.data; /*yaw angle en modo LOCAL_POSE DE UAL*/
+            }
+            else{
+                twist.twist.angular.z = joy_yaw_rec.data; /*yawrate*/
+                /*attitude_rate.body_rate.z = joy_z_rec.data;  /*yaw rate en modo ATTITUDE de UAL*/
+            }
+
+            /*LECTURA FLAG DE COORDENADAS: rosparam set /ual/pose_frame_id*/
+            c = r/2;
+            r = r%2;
+            
+            if(c == 0){ /*frame GROUND ENU*
+                /*pose.header.frame_id = '';*/
+                /*attitude.header.frame_id = ;*/
+                /*twist.header.frame_id = ;*/
+            }
+            else{ /*frame BODY FLU*/
+               /*pose.header.frame_id = '';*/
+                /*attitude.header.frame_id = ;*/
+                /*twist.header.frame_id = ;*/
+            }
+
+            /*LECTURA FLAG ACTIVE BREAK*/
+
+            if(r == 0){} /*ACTIVE BREAK DESACTIVADO*/
+            else{} /*ACTIVE BREAK ACTIVADO (por defecto en UAL*/
+
+        }
+        else if(ctrl_flag == 1){ /*para /dji_sdk/flight_control_setpoint_ENUposition_yaw*/
+            ROS_INFO("Controlando en posicion...");
+            pose.pose.position.x = current_pose.pose.position.x + joy_x_rec.data; /*X offset*/
+            pose.pose.position.y = current_pose.pose.position.y + joy_y_rec.data; /*Y offset*/
+            pose.pose.position.z = joy_z_rec.data; /*height*/
+            pose.pose.orientation.z = current_pose.pose.orientation.z + joy_yaw_rec.data; /*yaw angle en modo LOCAL_POSE DE UAL*/
+            local_pos_pub.publish(pose);          
         }
 
-        else if(ctrl_flag == 1) {
-            twist.twist.linear.x = joy_x.data; /*joy_command.axes[0]*/
-            twist.twist.linear.y = joy_y.data;
-            twist.twist.linear.z = 0.1;
+        else if(ctrl_flag == 2) { /*para /dji_sdk/flight_control_setpoint_ENUvelocity_yawrate*/
+            ROS_INFO("Controlando en velocidad...");
+            twist.twist.linear.x = joy_x_rec.data; /*velocidad en x*/
+            twist.twist.linear.y = joy_y_rec.data; /*velocidad en y*/
+            twist.twist.linear.z = joy_z_rec.data; /*velocidad en z*/
+            twist.twist.angular.z = joy_yaw_rec.data; /*yawrate*/
             local_vel_pub.publish(twist);
         }
 
-        else{
-            orientation.pose.orientation.x = orientation.pose.orientation.x + joy_x.data; /*joy_command.axes[0]*/
-            orientation.pose.orientation.y = orientation.pose.orientation.y + joy_y.data;
-            pose.pose.position.z = 2;    
-            local_attitude_pub.publish(orientation);
-            ros::spinOnce();
-            rate.sleep();
-            local_pos_pub.publish(pose);
+        else if (ctrl_flag == 3) { /*para /dji_sdk/flight_control_setpoint_rollpitch_yawrate_zposition*/
+            ROS_INFO("Controlando en angulo...");
+            attitude.orientation.x = current_pose.pose.orientation.x + joy_x_rec.data; /*roll*/
+            attitude.orientation.y = current_pose.pose.orientation.y + joy_y_rec.data; /*pitch*/
+            pose.pose.position.z = current_pose.pose.position.z + joy_z_rec.data;    /*height*/
+            attitude_rate.body_rate.z = joy_yaw_rec.data;  /*yaw rate en modo ATTITUDE de UAL*/
+            attitude.thrust = 0.5; /*necesito especificar este valor en UAL para controlar en angulo*/
+            attitude_rate.thrust = attitude.thrust; 
+            local_attitude_pub.publish(attitude);
+            /*local_attitude_rate_pub.publish(attitude_rate);*/
             }
+        else if (ctrl_flag == 4){
+            if(takeoff_client.call(takeoff_req, takeoff_resp)){
+                ROS_INFO("Takeoff Success");
+                ctrl_flag = 5;
+            }
+        }
         ros::spinOnce();
         rate.sleep();
     }
 
     return 0;
 }
-
